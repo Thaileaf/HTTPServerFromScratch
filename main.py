07a7a4680e1560b617ef09e7a29682213c0319e1
Thaileaf <lethaison2019@gmail.com>
2025-07-21 19:41:51 -0700

    Added multiprocessing and packet sniffing

--- main.py
+++ main.py
@@ -2,57 +2,102 @@ import socket
 import signal
 import sys
 # import time
+import multiprocessing
+from scapy.all import sniff, TCP, IP
+import os
 
-# Define the host and port
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 8080
+packet_count = 0
 
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def run_server():
+    print(f"[Server Process, PID: {multiprocessing.current_process().pid}] Starting up...")
 
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_socket.bind((SERVER_HOST, SERVER_PORT))
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-server_socket.listen(5)
+    server_socket.bind((SERVER_HOST, SERVER_PORT))
 
-print(f'Listening on port {SERVER_PORT} ...') # if all goes well, we will print that we have started listening on a specific port
+    server_socket.listen(5)
 
-# Clean up socket
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    server_socket.close()
-    sys.exit(0)
+    print(f'Listening on port {SERVER_PORT} ...') # if all goes well, we will print that we have started listening on a specific port
 
-signal.signal(signal.SIGINT, signal_handler)
+    # Clean up socket
+    def signal_handler(sig, frame):
+        print('You pressed Ctrl+C!')
+        server_socket.close()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
 
 
 
-while True: 
+    while True: 
+        
+        client_socket, client_address = server_socket.accept()
+        
+        request = client_socket.recv(1024).decode()
+        print(request)
+        headers = request.split('\n')
+        first_header_components = headers[0].split()
+
+        http_method = first_header_components[0]
+        path = first_header_components[1]
+
+        if http_method == 'GET':
+
+            content = "Hello\r\n"
+            print("Sending response: " + content)
+            response = 'HTTP/1.1 200 OK\r\n\r\n' + content
+        else:
+            response = 'HTTP/1.1 405 Method Not Allowed\r\n\r\nAllow: GET'
+
+        client_socket.sendall(response.encode())
+
+        client_socket.close()
+
+
+
+def packet_handler(packet):
+    """
+    When packet is found
+    """
+    global packet_count
+    packet_count += 1    
     
-    client_socket, client_address = server_socket.accept()
-    
-    request = client_socket.recv(1024).decode()
-    print(request)
-    headers = request.split('\n')
-    first_header_components = headers[0].split()
+    # Get the TCP layer to see payload size
+    if packet.haslayer(TCP):
+        tcp_layer = packet[TCP]
+        ip_layer = packet[IP]
+        source, dest = ip_layer.src, ip_layer.dst
 
-    http_method = first_header_components[0]
-    path = first_header_components[1]
+        direction = ""
+        if tcp_layer.dport == SERVER_PORT:
+            direction = "Request"
+        elif tcp_layer.sport == SERVER_PORT:
+            direction = "Response"
 
-    if http_method == 'GET':
-
-        content = "Hello\r\n"
-        print("Sending response: " + content)
-        response = 'HTTP/1.1 200 OK\r\n\r\n' + content
+        payload_size = len(tcp_layer.payload)
+        if payload_size > 0:
+            print(f"{direction} Packet #{packet_count}: Seq={packet[TCP].seq}, {source} -> {dest}, Payload Size={payload_size} bytes")
     else:
-        response = 'HTTP/1.1 405 Method Not Allowed\r\n\r\nAllow: GET'
+        print(f"Packet #{packet_count}: {packet.summary()}")
 
-    client_socket.sendall(response.encode())
+def packet_counter():
+    print(f"[Sniffer Process, PID: {multiprocessing.current_process().pid}] Starting up...")
 
-    client_socket.close()
+    bpf_filter = f"tcp and src port {SERVER_PORT}"
+    sniff(iface="lo", filter=bpf_filter, prn=packet_handler, store=0)
 
 
+if __name__ == "__main__":
+    print(f"Main process {os.getpid()} starting")
+    server_process = multiprocessing.Process(target=run_server)
+    packet_process = multiprocessing.Process(target=packet_counter)
 
+    # Start both processes
+    server_process.start()
+    packet_process.start()
 
-signal.pause()
