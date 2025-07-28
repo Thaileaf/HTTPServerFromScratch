7aa384e8fc9f5ea7585355db56bceaf3910c9494
Thaileaf <lethaison2019@gmail.com>
2025-07-28 03:12:48 -0700

    Basic 200 request working, time to handle 404 errors next

--- httpserver_v2.py
+++ httpserver_v2.py
@@ -79,15 +79,25 @@ import re
 
 # # class Headers
 
+STATUSES = {
+    200: "200 OK",
+    404: "404 Not Found"
+}
+
+DEBUG = True
+
 class HTTPServer:
-    def __init__(self, listen_ip, listen_port, listen_queue):
+    def __init__(self, listen_ip: str, listen_port: int, 
+                 paths: dict[str, str], listen_queue=5):
+        self.server_ip = listen_ip
+        self.server_port = listen_port
         self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.listen_socket.bind((listen_ip, listen_port))
         self.listen_socket.listen(listen_queue)
 
         self.packet_count = 0
-        self.paths = {}
+        self.paths = paths
         
 
         self.connections = [] # All accepted sockets
@@ -98,14 +108,15 @@ class HTTPServer:
     # def start(self):
     #     pass
     def serveForever(self):
+        print(f"Starting server for {self.server_ip}:{self.server_port}")
         while True:
             client_socket, client_address = self.listen_socket.accept()
+            print(f"Connection initiated from {client_address}")
 
-
-            try:
-                self.handleRequest(client_socket)
-            except Exception as e:
-                print("Whoops request failed")
+            # try:
+            self.handleRequest(client_socket)
+            # except Exception as e:
+            #     print("Whoops request failed", e)
 
                     
 
@@ -126,9 +137,10 @@ class HTTPServer:
 
         # Split data appropriately between body headers
         split_incoming = incoming.split(CLRF, 1)
+        print(split_incoming)
 
-        raw_headers.append(split_incoming[0])
-        raw_body.append(split_incoming[1])
+        raw_headers.extend(split_incoming[0])
+        raw_body.extend(split_incoming[1])
 
         # Decode Headers
         headers = raw_headers.decode().split('\n')
@@ -137,31 +149,53 @@ class HTTPServer:
         request_line_arr = headers[0].split(" ")
         if len(request_line_arr) != 3:
                 raise Exception("Malformed HTTP request line")
-        method, path, version = request_line_arr
         
-
+        method, path, version = request_line_arr # Version not used yet...
+        
+        # Validations
         self.validatePath(path)
-        
         parsed_headers = self.parseReqHeaders(headers[1:])
 
+        # Finally Handling the request
+
         if method == "GET":
-            self.handleGET(path, parsed_headers)
+            self.handleGET(path, client_socket, parsed_headers)
 
         elif method == "POST":
             self.handlePOST(path, parsed_headers)
 
-    def handleGET(self, path, headers):
-        pass
+    def handleGET(self, path, client_socket, headers):
+        headers = {
+            "Test1": "Test",
+            "Test2": "Test"
+        }
+        response = self.craftResponse(200, headers, "What's up")
+
+        if DEBUG: print("GET Response is:", response)
+
+        client_socket.sendall(response)
+        client_socket.close()
+
+
 
     def handlePOST(self, path, headers):
         pass
 
-    def craftResponse(self, code, headers: dict[str,str], content):
-        
-        for headers
-        headers = f"Content-Length: {len(content)}\r\n"
-        print("Sending response: " + content)
-        response = f'HTTP/1.1 200 OK\r\n{headers}\r\n' + content
+    def craftResponse(self, code: int, headers: dict[str,str], content):
+        '''
+        Craft responses for requests. Does not need to pass in content length header, calcs by default
+        '''
+        # sending_headers = ...
+        content_bytes = content.encode('utf-8')
+        headers["Content-Length"] = str(len(content_bytes))
+        header_lines = []
+        for key, value in headers.items():
+            header_lines.append(f"{key}: {value}")
+        header_str = "\r\n".join(header_lines)
+            
+        print("Crafting response: " + content)
+        response = (f'HTTP/1.1 {STATUSES[code]}\r\n{header_str}\r\n\r\n').encode('utf-8') + content_bytes
+        return response
         
     
 
@@ -184,6 +218,7 @@ class HTTPServer:
         pass
 
     def close(self):
+        print("Closing server...")
         self.listen_socket.close()
 
     # def __del__(self):
@@ -195,51 +230,56 @@ class HTTPServer:
 
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 8080
-packet_count = 0
+# packet_count = 0
 
 def run_server():
-    print(f"[Server Process, PID: {multiprocessing.current_process().pid}] Starting up...")
+    # print(f"[Server Process, PID: {multiprocessing.current_process().pid}] Starting up...")
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    server_socket.bind((SERVER_HOST, SERVER_PORT))
+    # server_socket.bind((SERVER_HOST, SERVER_PORT))
 
-    server_socket.listen(5)
+    # server_socket.listen(5)
 
-    print(f'Listening on port {SERVER_PORT} ...') # if all goes well, we will print that we have started listening on a specific port
+    # print(f'Listening on port {SERVER_PORT} ...') # if all goes well, we will print that we have started listening on a specific port
 
     # Clean up socket
+    server = HTTPServer(SERVER_HOST, SERVER_PORT,paths={"/":"True:)"})
     def signal_handler(sig, frame):
         print('You pressed Ctrl+C!')
-        server_socket.close()
+        server.close()
         sys.exit(0)
 
     signal.signal(signal.SIGINT, signal_handler)
 
+    server.serveForever()
 
 
-    while True: 
+    # while True: 
         
-        client_socket, client_address = server_socket.accept()
+    #     client_socket, client_address = server_socket.accept()
         
-        request = client_socket.recv(1024).decode()
-        print(request)
-        headers = request.split('\n')
-        first_header_components = headers[0].split()
+    #     request = client_socket.recv(1024).decode()
+    #     print(request)
+    #     headers = request.split('\n')
+    #     first_header_components = headers[0].split()
 
-        http_method = first_header_components[0]
-        path = first_header_components[1]
+    #     http_method = first_header_components[0]
+    #     path = first_header_components[1]
 
-        if http_method == 'GET':
+    #     if http_method == 'GET':
 
-            content = "Hello\r\n"
-            print("Sending response: " + content)
-            response = 'HTTP/1.1 200 OK\r\n\r\n' + content
-        else:
-            response = 'HTTP/1.1 405 Method Not Allowed\r\n\r\nAllow: GET'
+    #         content = "Hello\r\n"
+    #         print("Sending response: " + content)
+    #         response = 'HTTP/1.1 200 OK\r\n\r\n' + content
+    #     else:
+    #         response = 'HTTP/1.1 405 Method Not Allowed\r\n\r\nAllow: GET'
 
-        client_socket.sendall(response.encode())
+    #     client_socket.sendall(response.encode())
 
-        client_socket.close()
+    #     client_socket.close()
+
+
+run_server()
